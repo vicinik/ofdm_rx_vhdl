2ee50d863a43dab24445f2ec3cd37b3197002052
Daniel Ferrari <daniel.ferrari@students.fh-hagenberg.at>
2019-01-10 13:57:08 +0100

    Cleaned up testbench for fine alignment.

--- src/grpRx/unitFineAlignment/src/tb_fine_alignment-ea.vhd
+++ src/grpRx/unitFineAlignment/src/tb_fine_alignment-ea.vhd
@@ -31,34 +31,24 @@ architecture Bhv of tbCoarseAlignment is
 	signal reset_n : std_ulogic := '1';
 	signal init : std_ulogic := '0';
 	
---	signal rx_data_i_in : signed(11 downto 0) := (others => '0');
---	signal rx_data_q_in : signed(11 downto 0) := (others => '0');
---	signal rx_data_in_valid : std_ulogic := '0';
-        signal rx_symbols_i_fft_in : signed(11 downto 0) := (others => '0');
-        signal rx_symbols_q_fft_in : signed(11 downto 0) := (others => '0');
-        signal rx_symbols_fft_valid: std_ulogic := '0';
-        signal rx_symbols_fft_start: std_ulogic := '0';
+	signal rx_symbols_i_fft_in : signed(11 downto 0) := (others => '0');
+	signal rx_symbols_q_fft_in : signed(11 downto 0) := (others => '0');
+	signal rx_symbols_fft_valid: std_ulogic := '0';
+	signal rx_symbols_fft_start: std_ulogic := '0';
 
-        signal rx_symbols_i_out : signed(11 downto 0) := (others => '0');
-        signal rx_symbols_q_out : signed(11 downto 0) := (others => '0');
-        signal rx_symbols_valid : std_ulogic := '0';
-        signal rx_symbols_start : std_ulogic := '0';
+	signal rx_symbols_i_out : signed(11 downto 0) := (others => '0');
+	signal rx_symbols_q_out : signed(11 downto 0) := (others => '0');
+	signal rx_symbols_valid : std_ulogic := '0';
+	signal rx_symbols_start : std_ulogic := '0';
 	
 	signal offset_inc : std_ulogic := '0';
 	signal offset_dec : std_ulogic := '0';
---	signal interp_mode : std_ulogic := '0';
---	signal delay : std_ulogic_vector(3 downto 0);
---	signal offset : std_ulogic_vector(3 downto 0);
---	signal rx_data_i_out : signed(11 downto 0) := (others => '0');
---	signal rx_data_q_out : signed(11 downto 0) := (others => '0');
---	signal rx_data_out_valid : std_ulogic := '0';
---	signal rx_data_symb_start : std_ulogic := '0';
 begin
 
     ------------------------------------------------------------------
     -- DUV
     ------------------------------------------------------------------
-  DUV: entity work.FineAlignment
+  	DUV: entity work.FineAlignment
     port map (
       sys_clk_i              => sys_clk,
       sys_rstn_i             => reset_n,
@@ -119,80 +109,63 @@ begin
 	TestcaseSequencer: process
 		variable vDetectionTime : time := (cTrainingsSymbolPosition * cSamplesPerSymbol) * cDataClkPeriod;
 	begin	
-	    -- init uvvm, set log files and enable log messages
-        set_log_file_name("FineAlignment.txt");
-        set_alert_file_name("FineAlignment_alert.txt");
-        enable_log_msg(ALL_MESSAGES);
+		-- init uvvm, set log files and enable log messages
+		set_log_file_name("FineAlignment.txt");
+		set_alert_file_name("FineAlignment_alert.txt");
+		enable_log_msg(ALL_MESSAGES);
 		
 		-- generate reset pulse
 		log(ID_LOG_HDR, "Start clock and generate reset");
-        enable_sys_clock <= true;
-        gen_pulse(reset_n, '0', 4 * cSysClkPeriod, BLOCKING, "Generate reset pulse");
+		enable_sys_clock <= true;
+		gen_pulse(reset_n, '0', 4 * cSysClkPeriod, BLOCKING, "Generate reset pulse");
+			
+		log(ID_LOG_HDR, "Check reset condition");
+		check_value(rx_symbols_i_out, "000000000000", ERROR, "I component of output data");
+		check_value(rx_symbols_q_out, "000000000000", ERROR,"Q component of output data");
+		check_value(rx_symbols_valid, '0', MATCH_EXACT, ERROR, "Output valid signal");
+		check_value(rx_symbols_start, '0', MATCH_EXACT, ERROR, "Output start signal");
+		check_value(offset_inc, '0', MATCH_EXACT, ERROR, "Increment");
+		check_value(offset_dec, '1', MATCH_EXACT, ERROR, "Decrement");
 		
-	log(ID_LOG_HDR, "Check reset condition");
---		check_value(interp_mode, '0', MATCH_EXACT, ERROR, "Interpolation mode");
---		check_value(delay, "0000", MATCH_EXACT, ERROR, "Delay output");
---		check_value(offset, "0000", MATCH_EXACT, ERROR, "Offset output");
---		check_value(rx_data_out_valid, '0', MATCH_EXACT, ERROR, "Ouput valid signal");
---		check_value(rx_data_symb_start, '0', MATCH_EXACT, ERROR, "Symbol start signal");
---		check_value(rx_data_i_out, "000000000000", ERROR, "I component of output data");
---		check_value(rx_data_q_out, "000000000000", ERROR, "Q component of output data");
-        check_value(rx_symbols_i_out, "000000000000", ERROR, "I component of output data");
-        check_value(rx_symbols_q_out, "000000000000", ERROR,"Q component of output data");
-        check_value(rx_symbols_valid, '0', MATCH_EXACT, ERROR, "Output valid signal");
-        check_value(rx_symbols_start, '0', MATCH_EXACT, ERROR, "Output start signal");
-        check_value(offset_inc, '0', MATCH_EXACT, ERROR, "Increment");
-        check_value(offset_dec, '1', MATCH_EXACT, ERROR, "Decrement");
-	
-	log(ID_LOG_HDR, "Fine tune samples");
-	log(ID_SEQUENCER, "Start OFDM signal transmission");
-	enable_ofdm_signal_generation <= true;
---	await_value(interp_mode, '1', vDetectionTime - cDataClkPeriod, vDetectionTime + cDataClkPeriod, ERROR, "Interpolation mode '1' when peak is detected");		
-		
-	log(ID_LOG_HDR, "Start of symbol and valid generation");
---		await_value(rx_data_symb_start, '1', 0 ns, cDataClkPeriod, ERROR, "Start of symbol active");
---		await_value(rx_data_symb_start, '0', 0.99*cSysClkPeriod, 1.01*cSysClkPeriod, ERROR, "Start of symbol inactive");
---		await_value(rx_data_out_valid, '1', 0 ns, cDataClkPeriod, ERROR, "Data valid active");
---		await_value(rx_data_out_valid, '0', 0.99*cSysClkPeriod, 1.01*cSysClkPeriod, ERROR, "Data valid inactive");		
---		await_value(rx_data_symb_start, '1', 0 ns, cSamplesPerSymbol * cDataClkPeriod, ERROR, "Next Start of symbol active");
-        await_value(rx_symbols_fft_start, '1', 0 ns, cDataClkPeriod, ERROR, "Start of symbol active");
-        await_value(rx_symbols_fft_start, '0', 0.99 * cSysClkPeriod, 1.01 * cSysClkPeriod, ERROR, "Start of symbol inactive");
-        await_value(rx_symbols_fft_valid, '1', 0 ns, cDataClkPeriod, ERROR, "Data valid active");
-        await_value(rx_symbols_fft_valid, '0', 0.99 * cSysClkPeriod, 1.01 * cSysClkPeriod, ERROR, "Data valid inactive");
-        await_value(rx_symbols_fft_start, '1', 0 ns, cSamplesPerSymbol * cDataClkPeriod, ERROR, "Next start of symbole active");
-        
-	log(ID_LOG_HDR, "Rx output data stream");		
+		log(ID_LOG_HDR, "Fine tune samples");
+		log(ID_SEQUENCER, "Start OFDM signal transmission");
+		enable_ofdm_signal_generation <= true;	
+			
+		log(ID_LOG_HDR, "Start of symbol and valid generation");
+		await_value(rx_symbols_fft_start, '1', 0 ns, cDataClkPeriod, ERROR, "Start of symbol active");
+		await_value(rx_symbols_fft_start, '0', 0.99 * cSysClkPeriod, 1.01 * cSysClkPeriod, ERROR, "Start of symbol inactive");
+		await_value(rx_symbols_fft_valid, '1', 0 ns, cDataClkPeriod, ERROR, "Data valid active");
+		await_value(rx_symbols_fft_valid, '0', 0.99 * cSysClkPeriod, 1.01 * cSysClkPeriod, ERROR, "Data valid inactive");
+		await_value(rx_symbols_fft_start, '1', 0 ns, cSamplesPerSymbol * cDataClkPeriod, ERROR, "Next start of symbole active");
+			
+		log(ID_LOG_HDR, "Rx output data stream");		
 		for i in 1 to cSamplesPerSymbol loop
 			log(ID_SEQUENCER, "OFDM sample " & integer'image(i));
-		--	check_value(rx_data_out_valid, '1', MATCH_EXACT, ERROR, "Data valid active for sample");
-		--	check_value(rx_data_i_in, rx_data_i_out, ERROR, "I part of sample");
-                        --	check_value(rx_data_q_in, rx_data_q_out, ERROR, "Q part of sample");
-                        check_value(rx_symbols_valid, rx_symbols_fft_valid ,MATCH_EXACT, ERROR, "Data valid active for sample");
-                        check_value(rx_symbols_start, rx_symbols_fft_start, MATCH_EXACT, ERROR, "Data start active for sample");
-                        check_value(rx_symbols_i_out, rx_symbols_i_fft_in, ERROR, "I component of data");
-                        check_value(rx_symbols_q_out, rx_symbols_q_fft_in, ERROR, "Q component of data");
+			check_value(rx_symbols_valid, rx_symbols_fft_valid ,MATCH_EXACT, ERROR, "Data valid active for sample");
+			check_value(rx_symbols_start, rx_symbols_fft_start, MATCH_EXACT, ERROR, "Data start active for sample");
+			check_value(rx_symbols_i_out, rx_symbols_i_fft_in, ERROR, "I component of data");
+			check_value(rx_symbols_q_out, rx_symbols_q_fft_in, ERROR, "Q component of data");
 
-                        if i >= 32 then -- check after 32 samples if phase is kept until next symbol
-                           check_value(offset_inc, '1',MATCH_EXACT, ERROR, "Offset increment");
-                           check_value(offset_dec, '0', MATCH_EXACT, ERROR, "Offset decrement");
-                        end if;
-                        
+			if i >= 32 then -- check after 32 samples if phase is kept until next symbol
+				check_value(offset_inc, '1',MATCH_EXACT, ERROR, "Offset increment");
+				check_value(offset_dec, '0', MATCH_EXACT, ERROR, "Offset decrement");
+			end if;
+						
 			log(ID_SEQUENCER, "");
 			
 			if i /= cSamplesPerSymbol then
 				wait for cDataClkPeriod;
 			end if;
 		end loop;
-        --await_value(rx_data_symb_start, '1', 0 ns, 1.01*cDataClkPeriod, ERROR, "Start of symbol active at next sample");
-        await_value(rx_symbols_fft_start, '1', 0 ns, 1.01*cDataClkPeriod, ERROR, "Start of symbol active at next sample");
-	
+		await_value(rx_symbols_fft_start, '1', 0 ns, 1.01*cDataClkPeriod, ERROR, "Start of symbol active at next sample");
+
 		log(ID_LOG_HDR, "Stop simulation");
 		enable_sys_clock <= false;
 		enable_ofdm_signal_generation <= false;
-        wait_num_rising_edge(sys_clk, 2);
+		wait_num_rising_edge(sys_clk, 2);
 
-        report_alert_counters(VOID);
-        wait; -- final wait
+		report_alert_counters(VOID);
+		wait; -- final wait
 	end process;
 
 end architecture;
